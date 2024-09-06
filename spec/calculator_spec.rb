bc42631e8e8b1a1a01ac790bdfe58a3221f6979d
Himesh Rupareliya <himesh.rupareliya@gmail.com>
2024-09-07 00:24:55 +0530

    expect add method

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -209,3 +209,9 @@ RSpec.describe Calculator do
   end
 
 end
+
+RSpec.describe "add" do
+  it "takes delimited string of numbers as argument and returns its sum" do
+    expect(sum("1,2,3")).to eq(6)
+  end
+end
