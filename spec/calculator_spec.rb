d0e2b65430b52f80f445ce64e4b320750bfe9d43
Himesh Rupareliya <himesh.rupareliya@gmail.com>
2024-09-06 20:28:29 +0530

    setup initializer expectations

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,4 +1,14 @@
 require '../calculator'
 
 RSpec.describe Calculator do
+
+  describe "constructor" do
+    it "it takes one parameter and created object of type calculator" do
+      expect{Calculator.new}.to raise_error(ArgumentError)
+      calculator = Calculator.new ""
+      expect(calculator).to be_kind_of(Calculator)
+    end
+
+  end
+
 end
